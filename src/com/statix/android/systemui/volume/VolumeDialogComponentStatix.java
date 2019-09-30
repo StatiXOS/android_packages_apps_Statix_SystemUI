4f93936a4959ab6ad2113233bda1b07ae4d01e9a
Chris Crump <chris@cypheros.co>
2019-09-30 16:47:42 -0400

    StatixSystemUI: Introduce user interface for Alert Sliders.
    Ported from OxygenOS and reworked for our alert slider
    implementation. We target AudioManager instead of Zen,
    icons are also the same as aosp and the dialog uses the
    material theme as well as support for our themes.
    
    To use, the alert slider config must be enabled. By default,
    the dialog shows on the left side. To move it to the right
    side, set the location config to 1.
    
    Change-Id: I0522b849a8aeb3af0936eec4c0def894931cc564
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

---
+++ src/com/statix/android/systemui/volume/VolumeDialogComponentStatix.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.statix.android.systemui.volume;
+
+import android.content.Context;
+import com.android.systemui.dagger.SysUISingleton;
+import com.android.systemui.demomode.DemoModeController;
+import com.android.systemui.keyguard.KeyguardViewMediator;
+import com.android.systemui.plugins.ActivityStarter;
+import com.android.systemui.plugins.VolumeDialog;
+import com.android.systemui.volume.VolumeDialogComponent;
+import com.android.systemui.plugins.VolumeDialogController;
+import com.android.systemui.volume.VolumeDialogControllerImpl;
+import com.android.systemui.statusbar.policy.ExtensionController;
+import com.android.systemui.tuner.TunerService;
+import com.android.systemui.plugins.PluginDependencyProvider;
+import android.view.WindowManager.LayoutParams;
+
+import com.android.systemui.R;
+
+import com.statix.android.systemui.tristate.TriStateUiController;
+import com.statix.android.systemui.tristate.TriStateUiControllerImpl;
+
+import javax.inject.Inject;
+
+@SysUISingleton
+public class VolumeDialogComponentStatix extends VolumeDialogComponent
+        implements TriStateUiController.UserActivityListener  {
+
+    private TriStateUiController mTriStateController;
+
+    @Inject
+    public VolumeDialogComponentStatix(Context context, KeyguardViewMediator keyguardViewMediator,
+            ActivityStarter activityStarter, VolumeDialogControllerImpl volumeDialogController,
+            DemoModeController demoModeController, PluginDependencyProvider pluginDependencyProvider,
+            ExtensionController extensionController, TunerService tunerService, VolumeDialog volumeDialog) {
+        super(context, keyguardViewMediator, activityStarter, volumeDialogController, demoModeController, pluginDependencyProvider, extensionController, tunerService, volumeDialog);
+        boolean hasAlertSlider = mContext.getResources().
+                getBoolean(R.bool.config_hasAlertSlider);
+        extensionController.newExtension(TriStateUiController.class)
+                .withPlugin(TriStateUiController.class)
+                .withDefault(this::createTristateImpl)
+                .withCallback(triStateUi -> {
+                    if (hasAlertSlider) {
+                        if (mTriStateController != null) {
+                            mTriStateController.destroy();
+                        }
+                        mTriStateController = triStateUi;
+                        mTriStateController.init(LayoutParams.TYPE_VOLUME_OVERLAY, this);
+                    }
+                }).build();
+    }
+
+    @Override
+    public void onTriStateUserActivity() {
+        onUserActivity();
+    }
+
+    protected TriStateUiController createTristateImpl() {
+        TriStateUiControllerImpl impl = new TriStateUiControllerImpl(mContext);
+        return impl;
+    }
+}
