b75e030ee7f503888e95a0071c3dcca653bba15f
Anay Wadhera <anay1018@gmail.com>
2022-08-16 14:24:27 +0900

    [TMP] StatixSystemUI: drop Google parts
    Change-Id: I57a901c3ed4614686577e42872dbd7906285b983

--- src/com/statix/android/systemui/dagger/SystemUIStatixBinder.java
+++ src/com/statix/android/systemui/dagger/SystemUIStatixBinder.java
@@ -35,8 +35,6 @@ import com.android.systemui.volume.VolumeUI;
 import com.android.systemui.wmshell.WMShell;
 import com.android.systemui.dagger.SysUISingleton;
 
-import com.google.android.systemui.columbus.ColumbusTargetRequestService;
-
 import com.statix.android.systemui.StatixServices;
 import com.statix.android.systemui.statusbar.dagger.StatixStatusBarModule;
 import com.statix.android.systemui.theme.ThemeOverlayControllerStatix;
@@ -231,12 +229,4 @@ public abstract class SystemUIStatixBinder {
     @IntoMap
     @ClassKey(StatixServices.class)
     public abstract SystemUI bindStatixServices(StatixServices sysui);
-
-    /**
-     * Inject into ColumbusTargetRequestService.
-     */
-    @Binds
-    @IntoMap
-    @ClassKey(ColumbusTargetRequestService.class)
-    public abstract Service bindColumbusTargetRequestService(ColumbusTargetRequestService activity);
 }
