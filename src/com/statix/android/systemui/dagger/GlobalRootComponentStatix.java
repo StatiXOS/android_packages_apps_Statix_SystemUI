4aea1ccc087cc32601e0adc38b5ccfb460fa16b6
Anay Wadhera <anay1018@gmail.com>
2022-08-17 11:08:27 +0900

    StatixSystemUI: update dagger to 13
    Change-Id: I687c9c7b0e17041c37d619705293f04225bbcee2

--- src/com/statix/android/systemui/dagger/GlobalRootComponentStatix.java
+++ src/com/statix/android/systemui/dagger/GlobalRootComponentStatix.java
@@ -9,20 +9,14 @@ import android.content.Context;
 
 import com.android.systemui.dagger.GlobalModule;
 import com.android.systemui.dagger.GlobalRootComponent;
-import com.android.systemui.dagger.WMModule;
-
-import com.android.systemui.util.concurrency.ThreadFactory;
 
 import javax.inject.Singleton;
 
-import dagger.BindsInstance;
 import dagger.Component;
 
 @Singleton
 @Component(modules = {
-        GlobalModule.class,
-        SysUISubcomponentModuleStatix.class,
-        WMModule.class})
+        GlobalModule.class})
 public interface GlobalRootComponentStatix extends GlobalRootComponent {
 
     @Component.Builder
