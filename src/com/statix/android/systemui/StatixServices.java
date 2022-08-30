2e5cfd4f1817fa35ba16b906d4c2c2cc89c142bb
jhenrique09 <jhsv09@gmail.com>
2022-08-30 08:47:57 -0300

    StatixSystemUI: Implement ambient music support
    Change-Id: I3f7bccf1ac7e41fc16fcac08d64d581ddd0340b9

--- src/com/statix/android/systemui/StatixServices.java
+++ src/com/statix/android/systemui/StatixServices.java
@@ -5,6 +5,7 @@
 
 package com.statix.android.systemui;
 
+import android.app.AlarmManager;
 import android.content.Context;
 
 import com.android.systemui.Dumpable;
@@ -13,8 +14,11 @@ import com.android.systemui.VendorServices;
 import com.android.systemui.assist.AssistManager;
 import com.android.systemui.dagger.SysUISingleton;
 import com.android.systemui.flags.FeatureFlags;
+import com.android.systemui.statusbar.phone.CentralSurfaces;
 import com.android.systemui.statusbar.policy.FlashlightController;
 
+import com.statix.android.systemui.ambient.AmbientIndicationContainer;
+import com.statix.android.systemui.ambient.AmbientIndicationService;
 import com.statix.android.systemui.elmyra.ElmyraService;
 import com.statix.android.systemui.smartpixels.SmartPixelsReceiver;
 
@@ -30,13 +34,17 @@ import dagger.Lazy;
 public class StatixServices extends VendorServices {
 
     private final ArrayList<Object> mServices = new ArrayList<>();
-    private AssistManager mAssistManager;
-    private FlashlightController mFlashlightController;
+    private final AlarmManager mAlarmManager;
+    private final AssistManager mAssistManager;
+    private final CentralSurfaces mCentralSurfaces;
+    private final FlashlightController mFlashlightController;
 
     @Inject
-    public StatixServices(Context context, AssistManager assistManager, FlashlightController flashlightController) {
+    public StatixServices(Context context, AlarmManager alarmManager, AssistManager assistManager, CentralSurfaces centralSurfaces, FlashlightController flashlightController) {
         super(context);
+        mAlarmManager = alarmManager;
         mAssistManager = assistManager;
+        mCentralSurfaces = centralSurfaces;
         mFlashlightController = flashlightController;
     }
 
@@ -46,6 +54,9 @@ public class StatixServices extends VendorServices {
         if (mContext.getPackageManager().hasSystemFeature("android.hardware.context_hub") && mContext.getPackageManager().hasSystemFeature("android.hardware.sensor.assist")) {
             addService(new ElmyraService(mContext, mAssistManager, mFlashlightController));
         }
+        AmbientIndicationContainer ambientIndicationContainer = (AmbientIndicationContainer) mCentralSurfaces.getNotificationShadeWindowView().findViewById(R.id.ambient_indication_container);
+        ambientIndicationContainer.initializeView(mCentralSurfaces);
+        addService(new AmbientIndicationService(mContext, ambientIndicationContainer, mAlarmManager));
     }
 
     @Override
