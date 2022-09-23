e57224de52e93b3ec1305720d4bbd1d2475c9759
Anay Wadhera <anay1018@gmail.com>
2022-09-22 23:03:16 -0400

    StatixSystemUI: Add alternate UDFPS touch provider
    Change-Id: Ic2fe617e54af57920b9f315a46b891c9a5e8ac28

---
+++ src/com/statix/android/systemui/biometrics/FingerprintExtProvider.kt
@@ -0,0 +1,25 @@
+package com.statix.android.systemui.biometrics
+
+import android.os.IBinder
+import android.os.ServiceManager
+
+import com.google.hardware.biometrics.fingerprint.IFingerprintExt
+
+import javax.inject.Inject
+
+class FingerprintExtProvider @Inject constructor() {
+    public fun getExtension(): IFingerprintExt? {
+        val binder: IBinder = ServiceManager.waitForDeclaredService(FINGERPRINT_EXT_SERVICE)
+        if (binder == null) {
+            return null
+        }
+
+        val fingerprintExt = IFingerprintExt.Stub.asInterface(binder.getExtension())
+        return fingerprintExt
+    }
+
+    companion object {
+        const val FINGERPRINT_EXT_SERVICE: String = "android.hardware.biometrics.fingerprint.IFingerprint/default"
+    }
+
+}
