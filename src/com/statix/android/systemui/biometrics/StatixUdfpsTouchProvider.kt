4279d3b296ba5d006c20d847f6f90b6f69c7b444
Anay Wadhera <anay1018@gmail.com>
2022-09-22 23:03:16 -0400

    StatixSystemUI: Add alternate UDFPS touch provider
    Change-Id: Ic2fe617e54af57920b9f315a46b891c9a5e8ac28

---
+++ src/com/statix/android/systemui/biometrics/StatixUdfpsTouchProvider.kt
@@ -0,0 +1,49 @@
+package com.statix.android.systemui.biometrics
+
+import android.os.RemoteException
+
+import com.android.systemui.biometrics.AlternateUdfpsTouchProvider
+
+import com.google.hardware.biometrics.fingerprint.IFingerprintExt
+
+import javax.inject.Inject
+
+class StatixUdfpsTouchProvider @Inject constructor(
+    private val fingerprintExtProvider: FingerprintExtProvider
+) : AlternateUdfpsTouchProvider {
+
+    private var fingerprintExt: IFingerprintExt? = null
+
+    private fun getFingerprintExt(): IFingerprintExt? {
+        val extension = fingerprintExt ?: fingerprintExtProvider.getExtension()
+        if (fingerprintExt == null) {
+            fingerprintExt = extension
+        }
+        return extension
+    }
+
+    override fun onPointerDown(pointerId: Long, x: Int, y: Int, minor: Float, major: Float) {
+        try {
+            getFingerprintExt()?.onPointerDown(pointerId, x, y, minor, major)
+        } catch (e: RemoteException) {
+            fingerprintExt = null
+        }
+    }
+
+    override fun onPointerUp(pointerId: Long) {
+        try {
+            getFingerprintExt()?.onPointerUp(pointerId)
+        } catch (e: RemoteException) {
+            fingerprintExt = null
+        }
+    }
+
+    override fun onUiReady() {
+        try {
+            getFingerprintExt()?.onUiReady()
+        } catch (e: RemoteException) {
+            fingerprintExt = null
+        }
+    }
+
+}
