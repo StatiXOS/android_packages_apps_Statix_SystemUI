03284d0cc4cbc6cec82049e0af4299ee03103454
Anay Wadhera <anay1018@gmail.com>
2022-10-14 23:06:19 -0400

    StatixSystemUI: Implement ElmyraService from kdrag0n
    Change-Id: I1fa46d5334e242c9f46ae80571360005d7a3a3f4

--- src/com/statix/android/systemui/dagger/SystemUIStatixBinder.java
+++ src/com/statix/android/systemui/dagger/SystemUIStatixBinder.java
@@ -6,12 +6,27 @@
 
 package com.statix.android.systemui.dagger;
 
+import com.android.systemui.CoreStartable;
 import com.android.systemui.keyguard.dagger.KeyguardModule;
 import com.android.systemui.recents.RecentsModule;
 import com.android.systemui.statusbar.dagger.CentralSurfacesModule;
 
+import com.statix.android.systemui.StatixServices;
+
+import dagger.Binds;
 import dagger.Module;
+import dagger.multibindings.ClassKey;
+import dagger.multibindings.IntoMap;
 
 @Module(includes = {RecentsModule.class, CentralSurfacesModule.class, KeyguardModule.class})
 public abstract class SystemUIStatixBinder {
+
+    /**
+     * Inject into StatixServices.
+     */
+    @Binds
+    @IntoMap
+    @ClassKey(StatixServices.class)
+    public abstract CoreStartable bindStatixServices(StatixServices sysui);
+
 }
