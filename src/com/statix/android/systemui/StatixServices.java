e3cda3897eca78be9700c10f85f301f2cca3d9a8
Anay Wadhera <anay1018@gmail.com>
2022-10-29 15:10:23 -0400

    StatixSystemUI: Implement ElmyraService from kdrag0n
    Change-Id: I1fa46d5334e242c9f46ae80571360005d7a3a3f4

--- src/com/statix/android/systemui/StatixServices.java
+++ src/com/statix/android/systemui/StatixServices.java
@@ -10,9 +10,12 @@ import android.content.Context;
 import com.android.systemui.Dumpable;
 import com.android.systemui.R;
 import com.android.systemui.VendorServices;
+import com.android.systemui.assist.AssistManager;
 import com.android.systemui.dagger.SysUISingleton;
 import com.android.systemui.flags.FeatureFlags;
+import com.android.systemui.statusbar.policy.FlashlightController;
 
+import com.statix.android.systemui.elmyra.ElmyraService;
 import com.statix.android.systemui.smartpixels.SmartPixelsReceiver;
 
 import java.io.FileDescriptor;
@@ -27,15 +30,22 @@ import dagger.Lazy;
 public class StatixServices extends VendorServices {
 
     private final ArrayList<Object> mServices = new ArrayList<>();
+    private AssistManager mAssistManager;
+    private FlashlightController mFlashlightController;
 
     @Inject
-    public StatixServices(Context context) {
+    public StatixServices(Context context, AssistManager assistManager, FlashlightController flashlightController) {
         super(context);
+        mAssistManager = assistManager;
+        mFlashlightController = flashlightController;
     }
 
     @Override
     public void start() {
         addService(new SmartPixelsReceiver(mContext));
+        if (mContext.getPackageManager().hasSystemFeature("android.hardware.context_hub") && mContext.getPackageManager().hasSystemFeature("android.hardware.sensor.assist")) {
+            addService(new ElmyraService(mContext, mAssistManager, mFlashlightController));
+        }
     }
 
     @Override
