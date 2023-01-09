573a8776968fa36d414badc0454358f1cf91a2c5
Sourajit Karmakar <sourajitk@statixos.com>
2023-01-09 07:02:57 +0000

    StatixSystemUI: Create a custom keyguard affordance module
    Change-Id: I24ea09fded630a62a794f00d94ce389cba9e6e60

---
+++ src/com/statix/android/systemui/keyguard/domain/quickaffordance/StatixKeyguardQuickAffordanceRegistryImpl.kt
@@ -0,0 +1,47 @@
+package com.statix.android.systemui.keyguard.domain.quickaffordance
+
+import com.android.systemui.keyguard.domain.model.KeyguardQuickAffordancePosition
+import com.android.systemui.keyguard.domain.quickaffordance.HomeControlsKeyguardQuickAffordanceConfig
+import com.android.systemui.keyguard.domain.quickaffordance.KeyguardQuickAffordanceConfig
+import com.android.systemui.keyguard.domain.quickaffordance.KeyguardQuickAffordanceRegistry
+import com.android.systemui.keyguard.domain.quickaffordance.KeyguardQuickAffordanceRegistryImpl
+import com.android.systemui.keyguard.domain.quickaffordance.QrCodeScannerKeyguardQuickAffordanceConfig
+import com.android.systemui.keyguard.domain.quickaffordance.QuickAccessWalletKeyguardQuickAffordanceConfig
+
+import javax.inject.Inject
+import kotlin.reflect.KClass
+
+class StatixKeyguardQuickAffordanceRegistryImpl
+@Inject
+constructor(
+    homeControls: HomeControlsKeyguardQuickAffordanceConfig,
+    quickAccessWallet: QuickAccessWalletKeyguardQuickAffordanceConfig,
+    qrCodeScanner: QrCodeScannerKeyguardQuickAffordanceConfig,
+) : KeyguardQuickAffordanceRegistry<KeyguardQuickAffordanceConfig> {
+    private val configsByPosition =
+        mapOf(
+            KeyguardQuickAffordancePosition.BOTTOM_START to
+                listOf(
+                    homeControls,
+                ),
+            KeyguardQuickAffordancePosition.BOTTOM_END to
+                listOf(
+                    quickAccessWallet,
+                    qrCodeScanner,
+                ),
+        )
+    private val configByClass =
+        configsByPosition.values.flatten().associateBy { config -> config::class }
+
+    override fun getAll(
+        position: KeyguardQuickAffordancePosition,
+    ): List<KeyguardQuickAffordanceConfig> {
+        return configsByPosition.getValue(position)
+    }
+
+    override fun get(
+        configClass: KClass<out KeyguardQuickAffordanceConfig>
+    ): KeyguardQuickAffordanceConfig {
+        return configByClass.getValue(configClass)
+    }
+}
