b4178b3366020b400384849a375145feba47959a
Anay Wadhera <anay1018@gmail.com>
2024-01-01 23:23:34 -0500

    StatixSystemUI: Reintroduce Android 11's power menu
    anayw2001 edits: Update to 14
    
    Change-Id: I3cf6f35f98b5c84031682612a348a71a7a599415

---
+++ src/com/statix/android/systemui/globalactions/GlobalActionsModule.kt
@@ -0,0 +1,22 @@
+package com.statix.android.systemui.globalactions
+
+import com.android.systemui.globalactions.GlobalActionsComponent
+import com.android.systemui.plugins.GlobalActions
+import dagger.Binds
+import dagger.Module
+
+/**
+ * A dagger [Module] that provides GlobalActions-related implementations to SystemUI (reboot menu, etc.)
+ */
+@Module
+interface GlobalActionsModule {
+
+    /** */
+    @Binds
+    abstract fun provideGlobalActions(controllerImpl: GlobalActionsImpl): GlobalActions
+
+    /** */
+    @Binds
+    abstract fun provideGlobalActionsManager(controllerImpl: GlobalActionsComponent): GlobalActions.GlobalActionsManager
+
+}
